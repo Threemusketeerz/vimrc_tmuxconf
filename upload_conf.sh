f2cd60c8a6061439c721b362a7af424bf3f4a1c4
Bjarke <work.bjarke@gmail.com>
2017-12-22 10:04:17 +0100

    Upload by script

--- upload_conf.sh
+++ upload_conf.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-url=https://github.com/threemusketeerz/vimrc
-
-cp -i $1 confs_for_git_push/
-cp -i $2 confs_for_git_push/
-
-if [[ ! -e .git ]]; then
-    printf '.git not found creating git rep'
-    git init
-    git remote add origin $url
-fi
+cp -i ~/$1 .
+cp -i ~/$2 .
 
 git add -A
 git commit -m 'Upload by script' 
-git remote origin master
+git push upstream master
